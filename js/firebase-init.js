67ea6b8e62170566a9fd36010d3296b878aa2375
imtanjilsarkar <im.tanjilsarkar@gmail.com>
2026-06-05 10:12:27 +0600

    add firebase and custom domain

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,7 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, sendPasswordResetEmail, sendEmailVerification } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// This file now uses the global Firebase objects from CDN
+// No imports needed – Firebase will be available via the `firebase` global
 
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAupee5VRVJIGhvW95HnDSumB-f_k7kWzU",
   authDomain: "curepad-rx.firebaseapp.com",
@@ -11,9 +11,15 @@ const firebaseConfig = {
   appId: "1:986173283901:web:1cd6c97c0d3908a5825454"
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-export { sendPasswordResetEmail, sendEmailVerification };
+// Export services (for use in other scripts)
+const auth = firebase.auth();
+const db = firebase.firestore();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+// Make them globally available
+window.auth = auth;
+window.db = db;
+window.googleProvider = googleProvider;
